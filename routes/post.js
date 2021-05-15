599dc076680cbb66f56ae803ab951e26fbce0bc2
adigioia17 <annmariedigioia@yahoo.com>
2021-05-15 17:13:27 -0400

    Added some updates and style

--- routes/post.js
+++ routes/post.js
@@ -4,14 +4,24 @@ const data = require('../data');
 const posts = data.postData;
 
 
-router.get('/:idNumber', async (req, res) => {
+router.get('/:idNumber', async(req, res) => {
     let retrievedPost = await posts.getPost(req.params.idNumber);
 
     const title = retrievedPost.name;
     //convert the date
     retrievedPost.postedDate = new Date(retrievedPost.postedDate).toLocaleString('English', { hour12: false });
     console.log(JSON.stringify(retrievedPost.comments));
-    res.render('page/viewrecipe', { title: title, post:retrievedPost, postId:req.params.idNumber, scriptFile: '<script src="/public/js/likes.js"></script><script src="/public/js/comments.js"></script>'});
+    res.render('page/viewrecipe', { title: title, post: retrievedPost, postId: req.params.idNumber, scriptFile: '<script src="/public/js/likes.js"></script><script src="/public/js/comments.js"></script>' });
+});
+
+router.post('/edit/:idNumber', async(req, res) => {
+    let retrievedPost = await posts.getPost(req.params.idNumber);
+
+    const title = retrievedPost.name;
+    //convert the date
+    retrievedPost.postedDate = new Date(retrievedPost.postedDate).toLocaleString('English', { hour12: false });
+    // console.log(JSON.stringify(retrievedPost.comments));
+    res.render('page/editRecipe', { title: title, post: retrievedPost, postId: req.params.idNumber, scriptFile: '<script src="/public/js/likes.js"></script><script src="/public/js/comments.js"></script>' });
 });
 
 
