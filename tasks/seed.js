472c9e12ce34ab3de7643bf3fde2c33a39c6ea45
bruceshenqqe <bruceshenqqeq@gmail.com>
2021-05-15 12:40:36 -0400

    Update seed.js

--- tasks/seed.js
+++ tasks/seed.js
@@ -69,7 +69,7 @@ const main = async() => {
 
     let whiskeySour = await posts.createPost({
         "type": "Drink",
-        "postedDate": new Date(new Date() - 10 * 60 * 60 * 1000),
+        "postedDate": new Date(new Date() - 10 * 60 * 60 * 1000 - 31213),
         "name": "Whiskey Sour",
         "author_id": annmarie._id,
         "servings": 1,
@@ -82,9 +82,9 @@ const main = async() => {
         ]
     });
 
-    let burger = await posts.createPost({
+    let cheeseBurger = await posts.createPost({
         "type": "Food",
-        "postedDate": new Date(new Date() - 15 * 60 * 60 * 1000),
+        "postedDate": new Date(new Date() - 15 * 60 * 60 * 1000 + 32312),
         "name": "Cheeseburger",
         "author_id": annmarie._id,
         "servings": 1,
@@ -97,20 +97,100 @@ const main = async() => {
         ]
     });
 
+    let lemonMartini = await posts.createPost({
+        "type": "Drink",
+        "postedDate": new Date(new Date() - 1.5 * 60 * 60 * 1000 -31291),
+        "name": "Lemon Martini",
+        "author_id": xianli._id,
+        "servings": 1,
+        "cook_time": 10,
+        "prepare_time": 10,
+        "ingredients": "2 teaspoons white sugar, 2 teaspoons warm water, 1 lemon, 2 fluid ounces vodka, 1 fluid ounce orange-flavored liqueur, ice, 1 teaspoon white sugar",
+        "steps": ["Combine 2 teaspoons sugar and warm water in a cocktail shaker; stir to dissolve sugar. Pour lemon juice, lemon peels, vodka, and orange liqueur into the shaker; add ice. Cover and shake vigorously.",
+            "Remove a lemon peel from the shaker and wipe the rim of a martini glass with the peel.",
+            "Dip the rim of the glass in 1 teaspoon sugar, or as needed. Strain martini into prepared glass."
+        ]
+    });
+    //comment : So easy to make and is not too tart
+    //comment: Can't wait to try it 
 
+    let whiteChocolateLatte = await posts.createPost({
+        "type": "Drink",
+        "postedDate": new Date(new Date() - 25 * 60 * 60 * 1000 + 63125),
+        "name": "White Chocolate Latte",
+        "author_id": xianli._id,
+        "servings": 2,
+        "cook_time": 5,
+        "prepare_time": 5,
+        "ingredients": "1 cups milk, 1 tablespoon heavy cream, 1/8 teaspoon vanilla extract, 1 tablespoon white sugar, 1/2 cup brewed espresso, 1/4 cup white chocolate chips, chopped ",
+        "steps": ["Combine the milk and cream in a saucepan, and whisk over high heat until hot and frothy. Remove from heat and stir in the vanilla and sugar.",
+            "Whisk together hot espresso and white chocolate chips in a mug until smooth.",
+            "If you are making two, pour half into another mug. Top with the frothy hot milk and stir to blend in the flavoring."
+        ]
+    });
+    //comment: Absolutely delicious!!!
+    //comment: Good. It's easy to make and turns out really well. 
+    //comment: although it was good I didn't find it great, too bitter for me.
 
+    let lemonChickenTenders = await posts.createPost({
+        "type": "Food",
+        "postedDate": new Date(new Date() - 40 * 60 * 60 * 1000 - 4530),
+        "name": "Lemon Chicken Tenders",
+        "author_id": xianli._id,
+        "servings": 4,
+        "cook_time": 20,
+        "prepare_time": 15,
+        "ingredients": "1/2 teaspoon paprika, 1/2 teaspoon salt, 1/4 teaspoon pepper, 3 large boneless, skinless chicken breasts, cut into 2-inch pieces, 2 eggs, slightly beaten, 1 cup Italian seasoned bread crumbs, 1/2 cup sugar, 1/2 cup lemon juice, 1 teaspoons curry powder",
+        "steps": ["Preheat oven to 400 degrees F. In a bowl, stir together paprika, salt, and pepper. Sprinkle seasoning over chicken pieces. Dip pieces in egg, then dredge in bread crumbs. Arrange chicken pieces in a single layer in an aluminum foil-lined 15x10-inch jelly roll pan.",
+            "Bake in preheated oven for 15 minutes, turning once. In a small saucepan over medium-low heat, stir together sugar, lemon juice, and curry powder, stirring until sugar dissolves, about 5 minutes.",
+            "Drizzle lemon sauce over chicken, return to oven, and bake 5 minutes more."
+        ]
+    });
 
+    //comment: I love that curry taste 
+    //comment: Too sweet for me
 
+    let sample = await posts.createPost({
+        "type": "Food",
+        "postedDate": new Date(new Date() - 32 * 60 * 60 * 1000 + 23313),
+        "name": "Oyakodon (Japanese Chicken and Egg Rice Bowl)",
+        "author_id": xianli._id,
+        "servings": 4,
+        "cook_time": 25,
+        "prepare_time": 15,
+        "ingredients": "2 cups uncooked rice, 4 cups water, 4 skinless, boneless chicken thighs, cut into small pieces, 1 onion, cut in half and sliced, 2 cups dashi stock, made with dashi powder, 1/4 cup soy sauce, 3 tablespoons mirin, 3 tablespoons brown sugar, 4 eggs",
+        "steps": ["Rinse the rice in 3 to 4 changes of water until the rinse water is almost clear, and drain off the rinse water. Bring the rice and 4 cups of water to a boil in a saucepan over high heat. Reduce heat to medium-low, cover, and simmer until the rice is tender and the liquid has been absorbed, 20 to 25 minutes.",
+            "Place the chicken in a nonstick skillet with a lid, and cook and stir over medium heat until the chicken is no longer pink inside and beginning to brown, about 5 minutes. Stir in the onion, and cook and stir until the onion is soft, about 5 more minutes. Pour in the stock, and whisk in soy sauce, mirin, and brown sugar, stirring to dissolve the sugar. Bring the mixture to a boil, and let simmer until slightly reduced, about 10 minutes.",
+            "Whisk the eggs in a bowl until well-beaten, and pour over the chicken and stock. Cover the skillet, reduce heat, and allow to steam for about 5 minutes, until the egg is cooked. Remove from heat. To serve, place 1 cup of cooked rice per bowl into 4 deep soup bowls, top each bowl with 1/4 of the chicken and egg mixture, and spoon about 1/2 cup of soup into each bowl."
+        ]
+    });
+
+    /*
+        let sample = await posts.createPost({
+        "type": "Drink",
+        "postedDate": new Date(new Date() - 25 * 60 * 60 * 1000),
+        "name": "name of the recipe ",
+        "author_id": xianli._id,
+        "servings": 2,
+        "cook_time": 5,
+        "prepare_time": 5,
+        "ingredients": "ingredients of the recipe  ",
+        "steps": ["step1",
+            "step2",
+            "step3"
+        ]
+    });
+    */
     await likes.createLike(ObjectId(whiskeySour._id).toString());
     await likes.createLike(ObjectId(whiskeySour._id).toString());
     await likes.createLike(ObjectId(whiskeySour._id).toString());
     await comments.createComment(ObjectId(whiskeySour._id).toString(), { rating: 5, comment: "This amazing" });
     await comments.createComment(ObjectId(whiskeySour._id).toString(), { rating: 4, comment: "This is pretty good" });
 
-    await likes.createLike(ObjectId(burger._id).toString());
-    await likes.createLike(ObjectId(burger._id).toString());
-    await comments.createComment(ObjectId(burger._id).toString(), { rating: 5, comment: "This amazing" });
-    await comments.createComment(ObjectId(burger._id).toString(), { rating: 3, comment: "This is pretty mediocre" });
+    await likes.createLike(ObjectId(cheeseBurger._id).toString());
+    await likes.createLike(ObjectId(cheeseBurger._id).toString());
+    await comments.createComment(ObjectId(cheeseBurger._id).toString(), { rating: 5, comment: "This amazing" });
+    await comments.createComment(ObjectId(cheeseBurger._id).toString(), { rating: 3, comment: "This is pretty mediocre" });
 
     console.log('Done seeding database');
 
