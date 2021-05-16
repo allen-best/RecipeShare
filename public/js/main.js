f992ea201a4c65b54d9c3e9f31364a06486387ab
diya <diya.yang@ufl.edu>
2021-05-15 23:57:21 -0400

    profile final

--- public/js/main.js
+++ public/js/main.js
@@ -1,17 +1,1 @@
 
-(function ($) {
-    // Let's start writing AJAX calls!
-
-    let form = $('#updateUserInfoForm');
-
-    $(document).ready(function (event) {
-        let cookie_name = $.session('cookie_name');
-        if ($('#recipeLink').val()) {
-            
-        }
-
-    });
-
-
-
-})(window.jQuery);
--- routes/recipeForm.js
+++ routes/recipeForm.js
@@ -39,6 +39,21 @@ router.post('/', async(req, res) => {
     }
 });
 
+router.get('/:id', async(req, res) => {
+    let postID = req.params.id;
+
+    try {
+        const userInfo = await posts.get(postID);
+
+        const title = "Edit an account on RecipeShare";
+        res.render('page/editRecipeform', { title: title, userInfo:userInfo });
+    } catch (e) {
+        res.status(404);
+        res.render('page/error');
+    }
+        
+});
+
 router.patch('/:id', async(req, res) => {
     try {
         console.log(req.body);
